901a4caa8926cdc526e0e8ca97f78f552cbbca54
David Vargas <dv.carrillo@yahoo.com>
2019-05-10 11:42:17 +0200

    Initial Commit

---
+++ Clear/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Clear
+//
+//  Created by David Vargas Carrillo on 10/05/2019.
+//  Copyright © 2019 David Vargas Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Clear/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Clear
+//
+//  Created by David Vargas Carrillo on 10/05/2019.
+//  Copyright © 2019 David Vargas Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
