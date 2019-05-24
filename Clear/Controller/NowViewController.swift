e58d9c6925883641f3dd398cb6c3a4c22ccd61b6
David Vargas <dv.carrillo@yahoo.com>
2019-05-24 12:01:11 +0200

    Added attributes and first functions to NowViewController

--- Clear/Controller/NowViewController.swift
+++ Clear/Controller/NowViewController.swift
@@ -7,17 +7,90 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-class NowViewController: UIViewController {
-
+class NowViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // Weather services constants
+    let API_KEY = "fcd47afc48625799d8f94e94495690f0"
+    let WEATHER_NOW_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let WEATHER_FORECAST_URL = "api.openweathermap.org/data/2.5/forecast/daily"
+    
+    let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
+    let weatherForecast = WeatherForecast()
+    
+    // IB Outlets for displaying data
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var weatherConditionBackground: UIImageView!
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var minMaxTemperatureLabel: UILabel!
+    @IBOutlet weak var weatherConditionLabel: UILabel!
+    
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var rainLabel: UILabel!
+    
+    @IBOutlet weak var forecastDay1Label: UILabel!
+    @IBOutlet weak var forecastDay1Condition: UIImageView!
+    @IBOutlet weak var forecastDay1MinTemperature: UILabel!
+    @IBOutlet weak var forecastDay1MaxTemperature: UILabel!
+    
+    @IBOutlet weak var forecastDay2Label: UILabel!
+    @IBOutlet weak var forecastDay2Condition: UIImageView!
+    @IBOutlet weak var forecastDay2MinTemperature: UILabel!
+    @IBOutlet weak var forecastDay2MaxTemperature: UILabel!
+    
+    @IBOutlet weak var forecastDay3Label: UILabel!
+    @IBOutlet weak var forecastDay3Condition: UIImageView!
+    @IBOutlet weak var forecastDay3MinTemperature: UILabel!
+    @IBOutlet weak var forecastDay3MaxTemperature: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        let forecast = WeatherForecast()
-        forecast.printInfo()
+        // Location manager set up
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+    // MARK: - Networking
+    /************************************************************************/
+    
+    // Makes a request to the given URL
+    func getData(from url: String, parameters: [String: String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Alamofire - Response form server successful")
+                
+                let responseJSON : JSON = JSON(response.result.value!)
+                // TODO: Call functions to process the response
+            }
+            else {
+                print("Alamofire - Error: \(String(describing: response.result.error))")
+                self.cityLabel.text = "No connection"
+            }
+        }
+    }
+    
+    // MARK: - Location Manager delegate methods
+    /************************************************************************/
+    
+    // Executed after location update
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]    // Get the latest location
+        if location.horizontalAccuracy {
+            
+        }
+    }
 
 }
 
